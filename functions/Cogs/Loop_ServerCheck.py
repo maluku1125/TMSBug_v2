fe5ff85c888c519ccf1a8f84e4dce076809c3762
maluku1125 <62375411+maluku1125@users.noreply.github.com>
2025-03-19 21:11:51 +0800

    v2.9.2
    all server up check function bugs fix

--- functions/Cogs/Loop_ServerCheck.py
+++ functions/Cogs/Loop_ServerCheck.py
@@ -2,10 +2,8 @@ import socket
 from discord.ext import commands, tasks
 import discord
 import json
-
-
-with open(f'C:\\Users\\User\\Desktop\\DiscordBotlog\\Function\\Guild_Function.json', 'r', encoding='utf-8') as f:
-    Guild_Function = json.load(f)
+import datetime
+import asyncio
 
 HOST = [
   "202.80.104.24",
@@ -15,6 +13,9 @@ HOST = [
   "202.80.104.28",
   "202.80.104.29"
 ]
+
+def get_now_HMS():
+    return datetime.datetime.now().strftime('%H:%M:%S')
 
 class Loop_ServerCheck(commands.Cog):
     def __init__(self, bot):
@@ -46,7 +47,11 @@ class Loop_ServerCheck(commands.Cog):
             ret[host] = "unknown error"
         else:
             ret[host] = "online"
-        return        
+        return     
+    
+    def load_guild_function(self):
+        with open(f'C:\\Users\\User\\Desktop\\DiscordBotlog\\Function\\Guild_Function.json', 'r', encoding='utf-8') as f:
+            return json.load(f)  
 
     @tasks.loop(minutes = 1)
     async def check_server_status(self):
@@ -68,7 +73,7 @@ class Loop_ServerCheck(commands.Cog):
 
     @tasks.loop(minutes = 1)
     async def server_up_check(self):
-        print("server_up_check...")
+        print(f"{get_now_HMS()}, server_up_check...")
 
         for h in HOST:    
             self.worker(h, self.ret)
@@ -79,21 +84,49 @@ class Loop_ServerCheck(commands.Cog):
 
             await self.bot.change_presence(activity=discord.Game(name="MapleStory")) 
 
-            if self.offline_count >= 30:             
+            if self.offline_count >= 30: 
+                channelsendcountsuccess = 0
+                channelsendcountfail = 0   
+                
+                Guild_Function = self.load_guild_function()
+                
                 for guild_id, guild_config in Guild_Function.items():
+                    
                     channel_id = guild_config['ServerCheck_Channel']
-                    channel = self.bot.get_channel(channel_id)
-                    channelsendcount = 0
-                    if channel is None:
-                        print(f"頻道ID {channel_id} 不存在或無效")
+                    if not channel_id:
+                        print(f"{get_now_HMS()}, Guild: {guild_id} dont have ServerCheck_Channel")
                         continue
+                    
+                    channel = self.bot.get_channel(channel_id)
+                    
+                    if channel is None:
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} message not sent")
+                        channelsendcountfail += 1
+                        continue
+                    
+                    if channel is None or not channel.permissions_for(channel.guild.me).send_messages:
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} has no permission")
+                        channelsendcountfail += 1
+                        continue
+                        
                     mention = guild_config.get('ServerCheck_mention')
-                    if mention and mention != "None":
-                        await channel.send(f"<@&{mention}> 登入口已開啟。")
-                        channelsendcount += 1
-                    else:
-                        await channel.send("登入口已開啟。")
-                        channelsendcount += 1
+                    
+                    try:
+                        if mention and mention != "None":
+                            await channel.send(f"<@&{mention}> 登入口已開啟。")
+                            print(f"{get_now_HMS()}, ChannelID: {channel_id} message sent successfully") 
+                            channelsendcountsuccess += 1
+                        else:
+                            await channel.send("登入口已開啟。")
+                            print(f"{get_now_HMS()}, ChannelID: {channel_id} message sent successfully") 
+                            
+                        await asyncio.sleep(1)
+                        channelsendcountsuccess += 1
+                        
+                    except Exception as e:
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} error: {e}")
+                        channelsendcountfail += 1
+                        continue
                         
                         
             self.offline_count = 0    
@@ -101,18 +134,19 @@ class Loop_ServerCheck(commands.Cog):
             self.server_up_check.cancel()
             self.server_down_check.start()
             print('Server turn online')
-            print(f" sent message to {channelsendcount} channel")
+            print(f"Successfully sent message to {channelsendcountsuccess} channel, {channelsendcountfail} channel failed")
             print("-"*30)
         else:              
             self.worker('www.google.com', self.google)
             if self.google['www.google.com'] == 'online':
                 self.offline_count += 1
+                
                 print(f'Server is offline {self.offline_count}')
                 print("-"*30)
      
-    @tasks.loop(minutes = 10)
+    @tasks.loop(minutes = 5)
     async def server_down_check(self):
-        print("server_down_check...")
+        print(f"{get_now_HMS()}, server_down_check...")
             
         for h in HOST:    
             self.worker(h, self.ret)
@@ -120,34 +154,62 @@ class Loop_ServerCheck(commands.Cog):
         server_online = any(status == 'online' for status in self.ret.values())
           
         if not server_online and self.server_status != 'offline':
-
+        # if server_online and self.server_status != 'offline':
+            
             await self.bot.change_presence(activity=discord.CustomActivity(name="TMS Server Offline"))
-
+            channelsendcountsuccess = 0
+            channelsendcountfail = 0
+            
+            Guild_Function = self.load_guild_function()
+            
             for guild_id, guild_config in Guild_Function.items():
                 channel_id = guild_config['ServerCheck_Channel']
-                channel = self.bot.get_channel(channel_id)
-                channelsendcount = 0
-                if channel is None:
-                    print(f"頻道ID {channel_id} 不存在或無效")
+                
+                if not channel_id:
+                    print(f"{get_now_HMS()}, Guild: {guild_id} dont have ServerCheck_Channel")
                     continue
+                
+                channel = self.bot.get_channel(channel_id)                
+                if channel is None:
+                    print(f"{get_now_HMS()}, ChannelID: {channel_id} message not sent")
+                    channelsendcountfail += 1
+                    continue
+                
+                if channel is None or not channel.permissions_for(channel.guild.me).send_messages:
+                    print(f"{get_now_HMS()}, ChannelID: {channel_id} has no permission")
+                    channelsendcountfail += 1
+                    continue
+                                
                 mention = guild_config.get('ServerCheck_mention')
-                if mention and mention != "None":
-                    await channel.send(f"MapleStory 登入口已關閉。")
-                    channelsendcount += 1
-                else:
-                    await channel.send("MapleStory 登入口已關閉。")
-                    channelsendcount += 1
+                
+                try:
+                    if mention and mention != "None":
+                        await channel.send(f"MapleStory 登入口已關閉。")
+                        # await channel.send(f"群發訊息測試中，打擾抱歉orz")
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} message sent successfully") 
+                    else:
+                        await channel.send("MapleStory 登入口已關閉。")
+                        # await channel.send(f"群發訊息測試中，打擾抱歉orz")
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} message sent successfully") 
+                                  
+                    channelsendcountsuccess += 1
+                    await asyncio.sleep(1)
+                    
+                except Exception as e:
+                        print(f"{get_now_HMS()}, ChannelID: {channel_id} error: {e}")
+                        channelsendcountfail += 1
+                        continue
                     
             self.server_status = 'offline'
             self.server_up_check.start()
             self.server_down_check.cancel()
             print('Server turn offline')
-            print(f" sent message to {channelsendcount} channel")
+            print(f"Successfully sent message to {channelsendcountsuccess} channel, {channelsendcountfail} channel failed")
             print("-"*30)
         else:
 
-            await self.bot.change_presence(activity=discord.Game(name="MapleStory"))             
-
+            await self.bot.change_presence(activity=discord.Game(name="MapleStory"))       
+            
             print(f'Server is online')
             print("-"*30)
 
